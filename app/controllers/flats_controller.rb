4bd9ce3b27903949631dbcf256bc89b888d25af2
RonanKer29 <kervella.ronan@hotmail.com>
2024-02-21 16:10:04 +0100

    start

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,31 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      flash[:success] = "Flat successfully created"
+      redirect_to @flat
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+end
